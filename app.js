d9ed3453a637d6ac442cfb273112cb95b95a7192
ms4411 <myselftry0o0@gmail.com>
2026-07-22 12:42:59 +0900

    경로 수정

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import cors from "cors"
 
 // 웹소켓 연결을 위한 내장 http 모듈 불러오기
 import { createServer } from "http"; 
-import setupSocket from "./game.js"; // 방금 만든 game.js 불러오기
+import setupSocket from "./webSoket/game.js"; // 방금 만든 game.js 불러오기
 
 const app=express()
 const PORT=3001
